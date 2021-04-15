13ea3ff5621d8abef05c16c56cdcd329346c3805
luowen <bigpao.luo@gmail.com>
2021-04-15 19:33:42 +0800

    xx

---
+++ codes/src/main/java/algorithm/BinarySearch.java
@@ -0,0 +1,16 @@
+package algorithm;
+
+/**
+ * @author: luowen <loovien@163.com>
+ * @created: 4/15/2021 6:15 PM
+ */
+public class BinarySearch {
+    private static int[] data = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void binarySearch(int num) {
+    }
+}
