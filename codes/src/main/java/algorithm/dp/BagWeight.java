53e7496ddb48610b8b8a6333cf902076c626db13
luowen <loovien@163.com>
2021-06-02 22:11:47 +0800

    add dynamic plan

---
+++ codes/src/main/java/algorithm/dp/BagWeight.java
@@ -0,0 +1,56 @@
+package algorithm.dp;
+
+public class BagWeight {
+    public static void main(String[] args) {
+
+        System.out.println((int) '1' - (int)'0');
+
+        int[] p1 = new int[]{2, 1, 1, 1, 2, 1};
+        int[] p2 = new int[]{6, 1, 1, 3, 2, 100};
+
+        bagWeight(p1, p2, 3);
+        System.out.println(dp(p1, p2, 3));
+
+    }
+
+    public static void bagWeight(int[] w, int[] v, int weight) {
+        if (w == null || v == null || w.length != v.length || w.length <= 0) {
+            throw new RuntimeException("参数异常");
+        }
+        int value = calc(w, v, 0, weight);
+        System.out.println(value);
+    }
+
+    public static int calc(int[] w, int[] v, int index, int weight) {
+        if (index == w.length) {
+            return 0;
+        }
+        int p1 = calc(w, v, index + 1, weight);
+        int p2 = 0;
+        if (weight - w[index] >= 0) {
+            p2 = v[index] + calc(w, v, index + 1, weight - w[index]);
+        }
+        return Math.max(p1, p2);
+    }
+
+    public static int dp(int[] w, int[] v, int bag) {
+        if (w == null || v == null || w.length != v.length || w.length == 0) {
+            return -1;
+        }
+        int length = w.length;
+        int[][] dp = new int[length + 1][bag + 1];
+
+        for (int i = length - 1; i >= 0; i--) {
+            for (int j = 0; j <= bag; j++) {
+                int p1 = dp[i + 1][j];
+                int p2 = 0;
+                int next = j - w[i] < 0 ? -1 : dp[i + 1][j - w[i]];
+                if (next >= 0) {
+                    p2 = v[i] + next;
+                }
+                dp[i][j] = Math.max(p1, p2);
+            }
+        }
+        return dp[0][bag];
+    }
+}
---
+++ codes/src/main/java/algorithm/dp/Conv2Str.java
@@ -0,0 +1,7 @@
+package algorithm.dp;
+
+public class Conv2Str {
+    public static void main(String[] args) {
+
+    }
+}
