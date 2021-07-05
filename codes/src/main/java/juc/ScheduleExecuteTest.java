ebe810ebdd7ac8e25be1ed5c372bd126df97a349
luowen <loovien@163.com>
2021-07-06 06:53:02 +0800

    add scheduling

---
+++ codes/src/main/java/juc/ScheduleExecuteTest.java
@@ -0,0 +1,47 @@
+package juc;
+
+import java.util.Date;
+import java.util.concurrent.*;
+
+/**
+ * created 7/5/2021 9:59 PM
+ *
+ * @author luowen <loovien@163.com>
+ */
+public class ScheduleExecuteTest {
+
+    private final static ScheduledExecutorService ses = Executors.newScheduledThreadPool(1);
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        ses.execute(() -> System.out.println(new Date()));
+
+        ScheduledFuture<?> schedule = ses.schedule(() -> {
+            System.out.println("==============");
+
+            return 100;
+
+        }, 0, TimeUnit.SECONDS);
+
+        System.out.println(schedule.get());
+
+        ses.scheduleAtFixedRate(() -> {
+
+            System.out.println("luowen=========");
+
+        }, 0, 1, TimeUnit.SECONDS);
+
+
+        ScheduledFuture<?> scheduledFuture = ses.scheduleWithFixedDelay(() -> {
+            System.out.println("denay ======");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, 0, 1, TimeUnit.SECONDS);
+
+        System.out.println(scheduledFuture.get());
+    }
+
+}
